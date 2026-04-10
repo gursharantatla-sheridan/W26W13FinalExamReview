82bec67d6315d42d7978d18119cd0a389d3abd28
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-04-10 09:31:51 -0400

    context class

---
+++ W26W13FinalExamReview/Data/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using W26W13FinalExamReview.Models;
+
+namespace W26W13FinalExamReview.Data
+{
+    // context class
+    public class ProductContext : DbContext
+    {
+        // establish connection - using a constructor
+
+
+
+        // entity sets - DbSet<> properties
+
+
+
+        // data seeding - override the OnModelCreating method
+
+    }
+}
