4b5c20ecc9a2051bff5b5c6716bf6c42879b296a
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-04-10 09:21:52 -0400

    models

---
+++ W26W13FinalExamReview/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace W26W13FinalExamReview.Models
+{
+    public class Category
+    {
+        // scalar properties
+        public int CategoryId { get; set; }  // primary key
+        public string? CategoryName { get; set; }
+
+        // navigation property
+        public ICollection<Product>? Products { get; set; }
+    }
+}
